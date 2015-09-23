be84da162e6359215c40b5399b417cac0ee35bb2
Daniel Paladino <gabrielmlyra@gmail.com>
2015-09-22 22:27:25 -0300

    Fixed

---
+++ MAC - Vayne/MAC-Vayne/Util/TSelector.cs
@@ -0,0 +1,32 @@
+﻿using EloBuddy;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using MAC_Vayne.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAC_Vayne.Util
+{
+    static class TSelector
+    {
+
+        public static Menu TargetMenu;
+
+        private static void initMenu(Menu config)
+        {
+            TargetMenu = config.AddSubMenu("Target Selector", "TargetSelecta");
+            TargetMenu.AddGroupLabel("Type");
+            TargetMenu.Add("drawDisable", new CheckBox("Turn off all drawings", false));
+        }
+
+        public static AIHeroClient getTarget()
+        {
+
+            return null;
+        }
+        
+    }
+}
