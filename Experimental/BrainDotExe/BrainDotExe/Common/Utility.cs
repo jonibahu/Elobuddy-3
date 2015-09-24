31296e5edaf1491e094d2a2d5a4a7f3921e53c6c
gmlyra <gabrielmlyra@gmail.com>
2015-09-24 11:42:45 -0300

    Kappa Comit

--- Experimental/BrainDotExe/BrainDotExe/Common/Utility.cs
+++ Experimental/BrainDotExe/BrainDotExe/Common/Utility.cs
@@ -5,6 +5,63 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+#region do not open this region
+/*
+i told ya
+
+
+                         `-:+//:::/+o++o+++/-.`                        
+                    `-:+sydmmmdyssyhhdmmmddddhyso/oo/:.`              
+               `.:+o+hhhhdmmmmdddmddddmmmmddmmdddmhhhhhy+-`           
+             -:ohdhhdhyydmmdhdmmddddmmmmdhhddmddmmdmmdyyyys+-         
+         `-+syshysydmhyyhhhhddddddhhhhhhdhdmmddmmmmhyyhhdhyyhy/`      
+       `-+syyohysyddhoysosydhdhyyddmdddddhmmmddmmmdhhddmmmmmmmmhy/`   
+     `-oyosyyhhsshhhsoosyhdmdyydmmmdmmmmmdmNmmmmmmhdmmmmmmmmmmNmmmy-  
+    ./shyyyhhhysyhhysyddmmmdydmmmddmmmmNmmmmmmmmmdmmmmmmmmmmmmmmmmNy` 
+  `-+syyhyyyhyhyhddhdddhhhdmddmmmmmmmmmmmmmmmmmmmdmmmmmmmmmmNNmNNmNd+ 
+  :oshyyyyyyyhhddmmddhhddmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmNNNmNNms 
+ .oyyhysyyhhhddmmmmmdmmmmmmmmmmmmmmmmmmmmmmmdddddmmmmmmmmmmmmNNNNNNd+ 
+ +yyyysshhdddmdmmmmmNmmmmmmmmmmmmmmmmmmmmddhhhyyyyhhddmmmmmmmmNmmmNmy 
+.yhhhhyddddmmmmmmmmNmNmmmmmmmmmmmmmmddhhysoooo++o+ossyhhdmmmmNNmmNmNd`
+oddddddmdmmmmmmmmNmmmmmmmmmmmmmmmmddhyo+//://///++++ooosyhdmmNNmNNNNm-
+dmmmmmmmmmmmmmmNNNNNNmmmmmmdddmmdhsso+/:::/://///+++++++oyhdmmNNNNNNm:
+mmmmmmmmmmmmmmmNmNNNmmmmmdhhhhhddo++//:::::://///++++++osyhhddmNmNNNm:
+mmmmmmmmmmNmmmNNmmmmmmmddhs+++oyy+///:::::::////+++++ooosyhhhhdmNNNNm:
+dmmmmmmmNmmmmmNNmmmmmmmdddds+//+os/+::--::://+++++++++oossyyyhhdmNNNm`
+ymmmmmNNmmmmmmmmdddddddhs+oos+:::/+/:---:://////+///+++oossyyyhhmmNNm`
++mmmmmNNNNmmmmmmmddhy+++/::-::------:---::///////::////+oossyyhhmmNNd 
+-dNmmNmmmmmmmmmmmdhs+:::-------------::://+++++///://++oossyyyhhdmNNh 
+ ymNmmNNmmmmmmmmho+::------..--::/++ossssssoooo++ooossyyyhhhhhhhhmNms 
+ -hNNmNmNmmmmmds/::--------::/oyyhdmmmmmddhysoossyhddmmdmmmmmdmmddmd:  KAPPA !
+ `/shmNmmmmmmmo/:----:/+oosyhhdddmmmmmmmmmdhsooyhdmmmmmmmmmmmmmmmdmy  
+ :///sdNmmmmmh------:+oyhddmddmmmmmmmmmmmmdho//ohmmmmNNNmmmmmmmmddds  
+ ohdyssdmmmmdo------:+oyhddddddddddddddhyyyo/-.-+mmmmmmmmmmmmmmdhhy+  
+ :ss+/+shmmmdo-----..--:/++osyyhhhhdddhyo++/:-..-yddddddddddddhhyyo-  
+ ./+//oddyhdh+:----.......--:/+oosssso+////::-..-+hdhhhhhhhhyyyysy+.  
+ `:::+yoo:-/+:::----...`````..--::::---:::-:--.`./shhyssssssoosssyo.  
+  `-::+:/-..--::::----.....```........-:::----.``-/syyso++oo+osyydo.  
+   `-:-..:-.-::::::-:-----.........--://::----.`.-:oyhysooooosshhdo`  
+    `.......-:::::::::::::::::::::/++o+:.....-...:/ossyyyyssyyyddmo`  
+     ````..-::::::::::::////////+osso++++so++////ooyhhhhhhhhhhhddd+   
+      .-...-::::::::::///+++++oosss+///+sydddddhhhddmddhhhdhhddddd/   
+      `/////:/::::::://///++oossso+::::://+osyhdmmmmmdddhhhhhdddmy:   
+        `..../+//::::///////+oo+///:////++ooosyhddmmddddhhhhhhhdm+.   
+             `o+///////////::////++oooooooossyhhhddddddddhysyhhdd:    
+              ./+++/+++/////:::/+syyyhhyyhhdddddmmmmmmmdhysyhddm+`    
+               `-+oooo++++++++/+++/::///+++ossyyhhhdhhhyyyhhddmy.     
+                 `/ssssosssssssso/:---::/+osyyyyhhhhhyyyddddmdy`      
+                   .shyyyyyhhhhys+/:://+oosyhhddddhhhhhhddmmh+        
+                    `+yhhhhhhhyyo+++/+oossssyyhhddhhhhhddmmy/         
+                      .:shhhyyssoo+++++++ooooossyyyysyhdmd+.          
+                        `-ohhhyyssso++//::://+ooosssyhdm+`            
+                           `+shhhhyhysssooossyyyyhhhdmd-              
+                              ./shhddddddddddddddmmmmy-               
+                                 `-/syhhyhyhhhhyyo+/.`         
+             
+*/
+
+#endregion
+
 namespace BrainDotExe.Common
 {
     /// <summary>
