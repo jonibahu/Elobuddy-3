1c7883e800b421bdf234946b381d6fd21240b1ba
Daniel Paladino <gabrielmlyra@gmail.com>
2015-09-24 00:06:43 -0300

    50% of Jungle Time

---
+++ Experimental/BrainDotExe/BrainDotExe/Draw/JungleTimers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+
+namespace BrainDotExe.Draw
+{
+    public static class JungleTimers
+    {
+        public static AIHeroClient _Player
+        {
+            get { return ObjectManager.Player; }
+        }
+
+        public static void DrawJungleTimers_OnDraw(EventArgs args)
+        {
+            
+        }
+
+    }
+}
--- Experimental/BrainDotExe/BrainDotExe/Util/Misc.cs
+++ Experimental/BrainDotExe/BrainDotExe/Util/Misc.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EloBuddy;
+using SharpDX;
+using Color = System.Drawing.Color;
 
 namespace BrainDotExe.Util
 {
-    class Misc
+    static class Misc
     {
+        public static void DrawLine(Vector3 start, Vector3 end, Color color, float thickness = 2f)
+        {
+            Drawing.DrawLine(Drawing.WorldToScreen(start), Drawing.WorldToScreen(start), thickness, color);
+        }
+
+        public static void DrawText(Vector3 pos, Color color, string text, int size = 15)
+        {
+            Drawing.DrawText(Drawing.WorldToScreen(pos), color, text, size);
+        }
     }
 }
