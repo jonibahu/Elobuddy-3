8b7245ca74d256ffb0d2ab15395db4bfa7d98dc0
gmlyra <gabrielmlyra@gmail.com>
2015-10-14 10:53:37 -0300

    Action manager

---
+++ OneForWeek/OneForWeek/Model/ActionQueueItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneForWeek.Model
+{
+    using System;
+
+    struct ActionQueueItem
+    {
+        public float Time;
+        public Func<bool> PreConditionFunc;
+        public Func<bool> ConditionToRemoveFunc;
+        public Action ComboAction;
+    }
+}
---
+++ OneForWeek/OneForWeek/Util/ActionManager/ActionManager.cs
@@ -0,0 +1,57 @@
+﻿using EloBuddy;
+using OneForWeek.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneForWeek.Util
+{
+    static class ActionManager
+    {
+        public static void EnqueueAction(ActionQueueList list, Func<bool> preCondition, Action comboAction, Func<bool> conditionToRemove)
+        {
+            list.Add(new ActionQueueItem()
+            {
+                Time = Game.Time,
+                PreConditionFunc = preCondition,
+                ComboAction = comboAction,
+                ConditionToRemoveFunc = conditionToRemove
+            });
+        }
+
+        public static bool ExecuteNextAction(ActionQueueList list)
+        {
+            if (list.Count > 0)
+            {
+                if (Game.Time > list[0].Time + 2F)
+                {
+                    list.Remove(list[0]);
+                    return true;
+                }
+
+                if (list[0].PreConditionFunc.Invoke())
+                {
+                    list[0].ComboAction.Invoke();
+                }
+
+                if (list[0].ConditionToRemoveFunc.Invoke() || Game.Time > list[0].Time + 1.5F)
+                {
+                    list.Remove(list[0]);
+                    if (list.Count > 0)
+                    {
+                        var nextItem = list[0];
+                        nextItem.Time = Game.Time;
+                        list[0] = nextItem;
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
